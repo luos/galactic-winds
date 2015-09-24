0fcec515a2df304423804e0869bd3fcd00348a43
Lajos Gerecs <lajos.gerecs@gmail.com>
2015-09-24 21:02:21 +0200

    ship control, planet stuff

--- ts/app.ts
+++ ts/app.ts
@@ -7,11 +7,16 @@ interface Window {
 }
 
 class GalacticWinds {
-    worldWidth: number = 4000;
-    worldHeight: number = 2000;
-    game: Phaser.Game;
+    worldWidth: number = 2000;
+    worldHeight: number = 1800;
+    public game: Phaser.Game;
     moving: Array<Moving> = [];
+    map: GameMap;
     private background;
+    ships: Array<Ship> = [];
+    needsUpdate: Array<NeedsUpdate> = [];
+    selected = null;
+    public dispatcher : Dispatcher;
 
     constructor(){
         this.moving = [];
@@ -20,21 +25,31 @@ class GalacticWinds {
             create: this.create.bind(this),
             update: this.update.bind(this)
         });
+        this.map = new GameMap( this.game )
+        document.getElementById('content').oncontextmenu =function(e){
+            e.preventDefault();
+            return false;
+        }
+        this.dispatcher = new Dispatcher( this );
     }
 
     preload(){
         this.game.load.image('logo', '/phaser-2.4.3/resources/Phaser Logo/2D Text/Phaser 2D Glow.png');
         this.game.load.image( Fleet.SHIP_ORANGE , 'images/ships/ship1_orange.png' );
+        this.game.load.image( Fleet.SHIP_BLUE , 'images/ships/ship2_blue.png' );
+        this.game.load.image( Planet.TYPE_EARTH , 'images/planets/planet_earth.png' );
         this.game.load.image( Background.SPACEBACKGROUND , Background.SPACEBACKGROUND );
     }
 
     create(){
         this.game.world.setBounds( 0, 0, this.worldWidth, this.worldHeight );
-        var background = new Background( this.game , Background.SPACEBACKGROUND);
+        var background = new Background( this , Background.SPACEBACKGROUND);
         background.add();
         this.background = background;
-        var ship = new Fleet( this.game );
-        this.game.add.existing( ship );
+        this.dispatcher.createShip(Fleet.SHIP_BLUE, 50, 100 )
+        this.dispatcher.createShip(Fleet.SHIP_ORANGE, 100, 50 )
+        this.dispatcher.createPlanet( Planet.TYPE_EARTH, 200,200 );
+
 
     }
 
@@ -45,69 +60,264 @@ class GalacticWinds {
         var elapsed =this.game.time.elapsed;
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
         {
-            game.camera.x -= camSpeed;
-
-            if (!game.camera.atLimit.x)
-            {
-                s.tilePosition.x += camSpeed;
-            }
+            this.map.moveX( - camSpeed );
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
         {
-            game.camera.x += camSpeed;
-
-            if (!game.camera.atLimit.x)
-            {
-                s.tilePosition.x -= camSpeed;
-            }
+            this.map.moveX( + camSpeed );
         }
 
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
         {
-            game.camera.y -= camSpeed;
+            this.map.moveY( - camSpeed );
 
-            if (!game.camera.atLimit.y)
-            {
-                s.tilePosition.y += camSpeed;
-            }
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
         {
-            game.camera.y += camSpeed;
-
-            if (!game.camera.atLimit.y)
-            {
-                s.tilePosition.y -= camSpeed;
-            }
+            this.map.moveY( + camSpeed );
         }
 
+        var elapsed = this.game.time.elapsed;
+        for( var i=0; i < this.needsUpdate.length ; i++){
+            this.needsUpdate[i].doUpdate( elapsed );
+        }
     }
-
 
 }
 
 window.onload = () => window.game = new GalacticWinds();
-
+interface NeedsUpdate{
+    doUpdate( elapsed: number);
+}
 interface Ship {}
 
-class Fleet extends Phaser.Sprite implements Ship{
+class Fleet extends Phaser.Sprite implements Ship, NeedsUpdate, Movable, Positionable {
     public static  SHIP_ORANGE = "ship1_orange";
+    public static  SHIP_BLUE = "ship1_blue";
+    private selector;
+    private speed = 1.5;
+    private direction = new Phaser.Point( 0,0 )
+    private isMoving = false;
+    private target: Phaser.Point;
 
-    constructor( game: Phaser.Game ){
-        super(game, 50, 60, Fleet.SHIP_ORANGE )
+    constructor(private _game: GalacticWinds, shipType: string ){
+        super(_game.game, 30, 30, shipType );
+        this.width = 25;
+        this.height = 25;
+        this.inputEnabled =true;
+        this.events.onInputDown.add( this.onClick.bind( this ) );
+        this.selector = new Selector();
+        this.anchor.set( 0.5, 0.5 );
+        this.onClick(null,null);
+    }
+
+    onClick(sprite, pointer){
+        this._game.dispatcher.shipClicked( this, pointer );
     }
 
 
+
+    getX(){
+        return this.x;
+    }
+    getY(){
+        return this.y;
+    }
+
+    getWidth(){
+        return this.width;
+    }
+
+    getHeight(){
+        return this.height;
+    }
+
+    rotate(){
+        this.angle = this.angle + 0.5;
+    }
+
+    setX( x ){
+        this.x=x;
+    }
+
+    setY( y ){
+        this.y =y;
+    }
+
+    setDirection( p ){
+        this.direction = p;
+        this.isMoving = true;
+        var da = this.direction.angle({x: -1, y: 0 }, false ) + 3.14/2;
+        this.rotation = da;
+    }
+
+
+    doUpdate(elapsed:number) {
+        if ( this.isMoving ){
+            this.x = this.x + this.direction.x * this.speed;
+            this.y = this.y + this.direction.y * this.speed;
+            this.selector.x =this.x;
+            this.selector.y = this.y;
+            var dist = this.target.distance( new Phaser.Point( this.x, this.y ) );
+            if ( dist < 10 ){
+                this.isMoving = false;
+            }
+        }
+    }
+
+    setTarget( t : Phaser.Point ){
+        this.target = t;
+    }
+}
+
+class Selector{
+    private sprite: Phaser.Circle;
+    constructor(){
+        this.sprite = new Phaser.Circle( 10 , 20 , 30 );
+    }
+
+    public setCenter( x, y ){
+        this.sprite.setTo( x, y, 30 );
+    }
+
+    public add( game : Phaser.Game ){
+        game.add.existing( this.sprite );
+    }
 }
 
 class Background{
     static SPACEBACKGROUND = "/images/spacebackground.jpg";
     public sprite;
-    constructor( private game:Phaser.Game, private key : string ){
+    constructor( private game:GalacticWinds, private key : string ){
 
     }
 
     public add(){
-        this.sprite = sprite = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height,  this.key );
+        this.sprite = this.game.game.add.tileSprite(0, 0, this.game.game.world.width, this.game.game.world.height,  this.key );
+        console.log( this.sprite );
+        this.sprite.inputEnabled = true;
+
+        this.sprite.events.onInputDown.add( this.onClick.bind( this ) );
+    }
+
+    public onClick(sprite, pointer): void {
+
+         this.game.dispatcher.backgroundClicked( pointer );
+    }
+}
+
+class GameMap {
+    static DEFAULT_CHANGE_X = 5;
+    static DEFAULT_CHANGE_Y = 5;
+
+    constructor(private game: Phaser.Game ) {
+        
+    }
+
+    setCenter( x, y ){
+
+    }
+
+    moveX( x: number = GameMap.DEFAULT_CHANGE_X ){
+        this.game.camera.x += x;
+
+    }
+
+    moveY( y: number = GameMap.DEFAULT_CHANGE_Y ){
+        this.game.camera.y += y;
+    }
+
+    move( x: number , y: number ){
+
+    }
+
+}
+
+interface Movable {
+    getX():number;
+    getY():number;
+    setDirection( direction : Phaser.Point );
+    setTarget( target: Phaser.Point );
+}
+
+interface Positionable{
+    setX( x : number );
+    setY( y : number );
+}
+
+
+class Dispatcher {
+    constructor( private game: GalacticWinds) {
+        
+    }
+
+    public move( what : Movable , x: number , y: number ){
+        var direction = new Phaser.Point(x - what.getX() , y - what.getY() );
+        direction.normalize();
+        what.setDirection(direction)
+        console.log("moving selected", x , y , direction )
+        what.setTarget( new Phaser.Point( x, y ) );
+    }
+
+    public moveSelected( x, y ){
+         this.move( this.game.selected, x, y );
+    }
+
+    public createObject( ){
+        
+        
+    }
+
+    public createShip( textureType: string, x:number, y:number ){
+        var ship = new Fleet( this.game, textureType );
+        this.game.ships.push( ship );
+        this.game.needsUpdate.push(ship);
+        this.game.game.add.existing( ship );
+        ship.setX( x );
+        ship.setY( y );
+    }
+
+    public backgroundClicked( pointer ) {
+         this.moveSelected( pointer.worldX, pointer.worldY );
+    }
+
+    public shipClicked( ship, pointer ) {
+         if ( this.game.selected == ship ){
+           return;
+         }
+         if ( this.game.selected ){
+            this.game.selected.selector.destroy();
+         }
+         this.game.selected = ship;
+
+        var widthPer2 = ship.getWidth() / 2;
+        var heightPer2 = ship.getHeight() / 2;
+        var g = this.game.game.add.graphics( ship.getX(), ship.getY() );
+        g.lineStyle(2, 0xffaacc );
+        g.drawCircle( 0 , 0 , 50 );
+        ship.selector = g;
+        console.log("clicked")
+    }
+
+    public createPlanet( typeName : string , x, y ) {
+         var p = new Planet( this.game, typeName );
+         p.setX( x );
+         p.setY( y );
+    }
+}
+
+class Planet implements Positionable {
+    public static  TYPE_EARTH="planet_earth.png";
+    public sprite;
+
+    constructor( game: GalacticWinds, typeName:string ) {
+        this.sprite = game.game.add.sprite(25,25,typeName );
+    }
+
+    setX(x){
+        this.sprite.x =x;
+    }
+    setY(y){
+        this.sprite.y = y;
     }
 }
--- www/app.js
+++ www/app.js
@@ -9,28 +9,40 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var GalacticWinds = (function () {
     function GalacticWinds() {
-        this.worldWidth = 4000;
-        this.worldHeight = 2000;
+        this.worldWidth = 2000;
+        this.worldHeight = 1800;
         this.moving = [];
+        this.ships = [];
+        this.needsUpdate = [];
+        this.selected = null;
         this.moving = [];
         this.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'content', {
             preload: this.preload,
             create: this.create.bind(this),
             update: this.update.bind(this)
         });
+        this.map = new GameMap(this.game);
+        document.getElementById('content').oncontextmenu = function (e) {
+            e.preventDefault();
+            return false;
+        };
+        this.dispatcher = new Dispatcher(this);
     }
     GalacticWinds.prototype.preload = function () {
         this.game.load.image('logo', '/phaser-2.4.3/resources/Phaser Logo/2D Text/Phaser 2D Glow.png');
         this.game.load.image(Fleet.SHIP_ORANGE, 'images/ships/ship1_orange.png');
+        this.game.load.image(Fleet.SHIP_BLUE, 'images/ships/ship2_blue.png');
+        this.game.load.image(Planet.TYPE_EARTH, 'images/planets/planet_earth.png');
         this.game.load.image(Background.SPACEBACKGROUND, Background.SPACEBACKGROUND);
     };
     GalacticWinds.prototype.create = function () {
         this.game.world.setBounds(0, 0, this.worldWidth, this.worldHeight);
-        var background = new Background(this.game, Background.SPACEBACKGROUND);
+        var background = new Background(this, Background.SPACEBACKGROUND);
         background.add();
         this.background = background;
-        var ship = new Fleet(this.game);
-        this.game.add.existing(ship);
+        this.dispatcher.createShip(Fleet.SHIP_BLUE, 50, 100);
+        this.dispatcher.createShip(Fleet.SHIP_ORANGE, 100, 50);
+        this.dispatcher.createPlanet(Planet.TYPE_EARTH, 200, 200);
     };
     GalacticWinds.prototype.update = function () {
         var game = this.game;
@@ -38,28 +50,20 @@ var GalacticWinds = (function () {
         var s = this.background.sprite;
         var elapsed = this.game.time.elapsed;
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            game.camera.x -= camSpeed;
-            if (!game.camera.atLimit.x) {
-                s.tilePosition.x += camSpeed;
-            }
+            this.map.moveX(-camSpeed);
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            game.camera.x += camSpeed;
-            if (!game.camera.atLimit.x) {
-                s.tilePosition.x -= camSpeed;
-            }
+            this.map.moveX(+camSpeed);
         }
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            game.camera.y -= camSpeed;
-            if (!game.camera.atLimit.y) {
-                s.tilePosition.y += camSpeed;
-            }
+            this.map.moveY(-camSpeed);
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-            game.camera.y += camSpeed;
-            if (!game.camera.atLimit.y) {
-                s.tilePosition.y -= camSpeed;
-            }
+            this.map.moveY(+camSpeed);
+        }
+        var elapsed = this.game.time.elapsed;
+        for (var i = 0; i < this.needsUpdate.length; i++) {
+            this.needsUpdate[i].doUpdate(elapsed);
         }
     };
     return GalacticWinds;
@@ -67,21 +71,179 @@ var GalacticWinds = (function () {
 window.onload = function () { return window.game = new GalacticWinds(); };
 var Fleet = (function (_super) {
     __extends(Fleet, _super);
-    function Fleet(game) {
-        _super.call(this, game, 50, 60, Fleet.SHIP_ORANGE);
+    function Fleet(_game, shipType) {
+        _super.call(this, _game.game, 30, 30, shipType);
+        this._game = _game;
+        this.speed = 1.5;
+        this.direction = new Phaser.Point(0, 0);
+        this.isMoving = false;
+        this.width = 25;
+        this.height = 25;
+        this.inputEnabled = true;
+        this.events.onInputDown.add(this.onClick.bind(this));
+        this.selector = new Selector();
+        this.anchor.set(0.5, 0.5);
+        this.onClick(null, null);
     }
+    Fleet.prototype.onClick = function (sprite, pointer) {
+        this._game.dispatcher.shipClicked(this, pointer);
+    };
+    Fleet.prototype.getX = function () {
+        return this.x;
+    };
+    Fleet.prototype.getY = function () {
+        return this.y;
+    };
+    Fleet.prototype.getWidth = function () {
+        return this.width;
+    };
+    Fleet.prototype.getHeight = function () {
+        return this.height;
+    };
+    Fleet.prototype.rotate = function () {
+        this.angle = this.angle + 0.5;
+    };
+    Fleet.prototype.setX = function (x) {
+        this.x = x;
+    };
+    Fleet.prototype.setY = function (y) {
+        this.y = y;
+    };
+    Fleet.prototype.setDirection = function (p) {
+        this.direction = p;
+        this.isMoving = true;
+        var da = this.direction.angle({ x: -1, y: 0 }, false) + 3.14 / 2;
+        this.rotation = da;
+    };
+    Fleet.prototype.doUpdate = function (elapsed) {
+        if (this.isMoving) {
+            this.x = this.x + this.direction.x * this.speed;
+            this.y = this.y + this.direction.y * this.speed;
+            this.selector.x = this.x;
+            this.selector.y = this.y;
+            var dist = this.target.distance(new Phaser.Point(this.x, this.y));
+            if (dist < 10) {
+                this.isMoving = false;
+            }
+        }
+    };
+    Fleet.prototype.setTarget = function (t) {
+        this.target = t;
+    };
     Fleet.SHIP_ORANGE = "ship1_orange";
+    Fleet.SHIP_BLUE = "ship1_blue";
     return Fleet;
 })(Phaser.Sprite);
+var Selector = (function () {
+    function Selector() {
+        this.sprite = new Phaser.Circle(10, 20, 30);
+    }
+    Selector.prototype.setCenter = function (x, y) {
+        this.sprite.setTo(x, y, 30);
+    };
+    Selector.prototype.add = function (game) {
+        game.add.existing(this.sprite);
+    };
+    return Selector;
+})();
 var Background = (function () {
     function Background(game, key) {
         this.game = game;
         this.key = key;
     }
     Background.prototype.add = function () {
-        this.sprite = sprite = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, this.key);
+        this.sprite = this.game.game.add.tileSprite(0, 0, this.game.game.world.width, this.game.game.world.height, this.key);
+        console.log(this.sprite);
+        this.sprite.inputEnabled = true;
+        this.sprite.events.onInputDown.add(this.onClick.bind(this));
+    };
+    Background.prototype.onClick = function (sprite, pointer) {
+        this.game.dispatcher.backgroundClicked(pointer);
     };
     Background.SPACEBACKGROUND = "/images/spacebackground.jpg";
     return Background;
+})();
+var GameMap = (function () {
+    function GameMap(game) {
+        this.game = game;
+    }
+    GameMap.prototype.setCenter = function (x, y) {
+    };
+    GameMap.prototype.moveX = function (x) {
+        if (x === void 0) { x = GameMap.DEFAULT_CHANGE_X; }
+        this.game.camera.x += x;
+    };
+    GameMap.prototype.moveY = function (y) {
+        if (y === void 0) { y = GameMap.DEFAULT_CHANGE_Y; }
+        this.game.camera.y += y;
+    };
+    GameMap.prototype.move = function (x, y) {
+    };
+    GameMap.DEFAULT_CHANGE_X = 5;
+    GameMap.DEFAULT_CHANGE_Y = 5;
+    return GameMap;
+})();
+var Dispatcher = (function () {
+    function Dispatcher(game) {
+        this.game = game;
+    }
+    Dispatcher.prototype.move = function (what, x, y) {
+        var direction = new Phaser.Point(x - what.getX(), y - what.getY());
+        direction.normalize();
+        what.setDirection(direction);
+        console.log("moving selected", x, y, direction);
+        what.setTarget(new Phaser.Point(x, y));
+    };
+    Dispatcher.prototype.moveSelected = function (x, y) {
+        this.move(this.game.selected, x, y);
+    };
+    Dispatcher.prototype.createObject = function () {
+    };
+    Dispatcher.prototype.createShip = function (textureType, x, y) {
+        var ship = new Fleet(this.game, textureType);
+        this.game.ships.push(ship);
+        this.game.needsUpdate.push(ship);
+        this.game.game.add.existing(ship);
+        ship.setX(x);
+        ship.setY(y);
+    };
+    Dispatcher.prototype.backgroundClicked = function (pointer) {
+        this.moveSelected(pointer.worldX, pointer.worldY);
+    };
+    Dispatcher.prototype.shipClicked = function (ship, pointer) {
+        if (this.game.selected == ship) {
+            return;
+        }
+        if (this.game.selected) {
+            this.game.selected.selector.destroy();
+        }
+        this.game.selected = ship;
+        var widthPer2 = ship.getWidth() / 2;
+        var heightPer2 = ship.getHeight() / 2;
+        var g = this.game.game.add.graphics(ship.getX(), ship.getY());
+        g.lineStyle(2, 0xffaacc);
+        g.drawCircle(0, 0, 50);
+        ship.selector = g;
+        console.log("clicked");
+    };
+    Dispatcher.prototype.createPlanet = function (typeName, x, y) {
+        var p = new Planet(this.game, typeName);
+        p.setX(x);
+        p.setY(y);
+    };
+    return Dispatcher;
+})();
+var Planet = (function () {
+    function Planet(game, typeName) {
+        this.sprite = game.game.add.sprite(25, 25, typeName);
+    }
+    Planet.prototype.setX = function (x) {
+        this.sprite.x = x;
+    };
+    Planet.prototype.setY = function (y) {
+        this.sprite.y = y;
+    };
+    Planet.TYPE_EARTH = "planet_earth.png";
+    return Planet;
 })();
 //# sourceMappingURL=app.js.map
